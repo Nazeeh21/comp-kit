a065e628a0f8a25a5b1e1ac306409734e808b21b
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-05-22 23:59:12 +0530

    fix linting errors & update structure

---
+++ package/src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react';
+
+export const Button = styled('button', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
--- package/src/index.ts
+++ package/src/index.ts
@@ -1,11 +1,1 @@
-import { styled } from '@stitches/react';
-
-export const Button = styled('button', {
-  backgroundColor: 'gainsboro',
-  borderRadius: '9999px',
-  fontSize: '13px',
-  padding: '10px 15px',
-  '&:hover': {
-    backgroundColor: 'lightgray',
-  },
-});
+export { Button } from './Button/Button';
