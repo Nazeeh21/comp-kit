900e25dde4846b30e768b0a1cf2cba2cf07b8636
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-05-24 00:49:07 +0530

    Add `change.yml` workflow

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { Button } from '../package/src';
 
-const Comp = () => <Button>Test Button</Button>;
+const Comp = () => <Button>Click me</Button>;
 
 const meta: Meta<typeof Comp> = {
   title: 'Button',
