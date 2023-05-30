b5993dd0ca7097bc243bb9069d617765641ba167
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-05-30 16:39:58 +0530

    Add `Select` comp

---
+++ package/src/components/Select/Select.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react';
+
+export const Select = styled('select', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '04rem',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
--- package/src/index.ts
+++ package/src/index.ts
@@ -1,6 +1,7 @@
 // Components
 export { Button } from './components/Button/Button';
 export { ConnectButton } from './components/ConnectButton/ConnectButton';
+export { Select } from './components/Select/Select';
 export {
   KitProvider,
   KitProviderProps,
