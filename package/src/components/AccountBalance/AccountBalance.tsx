b04420d13ed53d213b66410fa0db3acf20f9680f
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-06-01 22:51:18 +0530

    Add `AccountBalance` component

--- package/src/components/AccountBalance/AccountBalance.tsx
+++ package/src/components/AccountBalance/AccountBalance.tsx
@@ -1,13 +1,45 @@
-import { Address } from 'viem';
+import React, { useState } from 'react';
+import { Address, getAddress } from 'viem';
+import { useBalance } from '../../hooks/useBalance';
 import { Input } from '../Input/Input';
-import React from 'react';
 
 export const AccountBalance = () => {
-  const [address, setAddress] = React.useState<Address>();
+  const [address, setAddress] = React.useState<Address | undefined>();
+  const [inValid, setInvalid] = useState<boolean>(false);
+
+  const { getBalance, etherBalance, loading, balance, error } = useBalance();
+
+  const onChangeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value as Address;
+    setAddress(newValue);
+    if (e.target.value === '') {
+      setInvalid(false);
+      return;
+    }
+    try {
+      const checksummedAddress = getAddress(newValue, 30);
+      if (checksummedAddress) {
+        await getBalance({ address: newValue });
+      }
+      setInvalid(false);
+    } catch (error) {
+      console.log('invalid address ', { error });
+      setInvalid(true);
+    }
+  };
+
   return (
-    <Input
-      value={address}
-      onChange={e => setAddress(e.target.value as Address)}
-    />
+    <div>
+      <Input value={address} onChange={onChangeHandler} />
+      {balance !== undefined && (
+        <div>Balance in gwei: {balance.toString()}</div>
+      )}
+      {etherBalance !== undefined && (
+        <div>Balance in ethers: {etherBalance}</div>
+      )}
+      {inValid && <div>Invalid address</div>}
+      {loading && <div>Loading...</div>}
+      {error && <div>{error.message}</div>}
+    </div>
   );
 };
--- package/src/hooks/useBalance/index.tsx
+++ package/src/hooks/useBalance/index.tsx
@@ -1,41 +1,40 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Address } from 'viem';
+import { useCallback, useState } from 'react';
+import { GetBalanceParameters, formatEther } from 'viem';
 import { usePublicClient } from '../../components/KitProvider/KitProvider';
-
 export interface UseBalanceResult {
+  getBalance: (args: GetBalanceParameters) => Promise<void>;
+  etherBalance?: string;
   balance?: bigint;
   loading: boolean;
   error?: Error;
 }
 
-export interface useBalanceProps {
-  address: Address;
-}
-
-export const useBalance = ({ address }: useBalanceProps): UseBalanceResult => {
+export const useBalance = (): UseBalanceResult => {
   const [balance, setBalance] = useState<bigint>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error>();
+
   const publicClient = usePublicClient();
 
-  const getBalance = useCallback(async () => {
-    setLoading(true);
-    setError(undefined);
-    try {
-      const balance = await publicClient?.getBalance({ address });
-      setBalance(balance);
-    } catch (error) {
-      setError(new Error(error as string));
-    } finally {
-      setLoading(false);
-    }
-  }, [publicClient, address]);
-
-  useEffect(() => {
-    void getBalance();
-  }, []);
+  const getBalance = useCallback(
+    async ({ address, ...args }: GetBalanceParameters) => {
+      setLoading(true);
+      setError(undefined);
+      try {
+        const balance = await publicClient?.getBalance({ address, ...args });
+        setBalance(balance);
+      } catch (error) {
+        setError(new Error(error as string));
+      } finally {
+        setLoading(false);
+      }
+    },
+    [publicClient]
+  );
 
   return {
+    getBalance,
+    etherBalance: balance ? formatEther(balance) : undefined,
     balance,
     loading,
     error,
