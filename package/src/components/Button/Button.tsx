01e677f0a39f096c43fbd33a4c6b25fcdf99acc6
miralsuthar <miralsuthar2000@gmail.com>
2023-06-01 23:45:36 +0530

    fix: button component styles and gave them varinats

--- package/src/components/Button/Button.tsx
+++ package/src/components/Button/Button.tsx
@@ -1,11 +1,43 @@
 import { styled } from '@stitches/react';
 
 export const Button = styled('button', {
-  backgroundColor: 'gainsboro',
-  borderRadius: '9999px',
-  fontSize: '13px',
+  backgroundColor: 'white',
+  borderRadius: '6px',
+  fontSize: '14px',
   padding: '10px 15px',
+  cursor: 'pointer',
+  border: 'none',
   '&:hover': {
-    backgroundColor: 'lightgray',
+    backgroundColor: '#f5f5f5',
+  },
+
+  variants: {
+    color: {
+      dark: {
+        backgroundColor: 'black',
+        color: 'white',
+        '&:hover': {
+          backgroundColor: '#1b1a1b',
+        },
+        '&:focus': {
+          outline: '#1b1a1b solid 2px',
+          outlineOffset: '1px',
+        },
+      },
+      light: {
+        backgroundColor: 'white',
+        color: 'black',
+        '&:hover': {
+          backgroundColor: '#f5f5f5',
+        },
+        '&:focus': {
+          outline: '#f5f5f5 solid 2px',
+          outlineOffset: '1px',
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    color: 'light',
   },
 });
