cbd17c6d30bcd6d57cf43280e41972b6ba7a171b
miralsuthar <miralsuthar2000@gmail.com>
2023-06-07 11:45:07 +0530

    fix: walletconnect error

--- package/src/components/Wallets/wallectConnectWallet.tsx
+++ package/src/components/Wallets/wallectConnectWallet.tsx
@@ -1,8 +1,13 @@
 import { EthereumProvider } from '@walletconnect/ethereum-provider';
+// import {} from '@walletconnect/web3-provider';
 import { createWalletClient, custom } from 'viem';
 import { mainnet } from 'viem/chains';
 import { useAddress } from '../../hooks/useAddress';
 import { useSetWalletClient } from '../KitProvider/KitProvider';
+
+const projectId = '5a13f1a5297da2cd768519079890e4fe';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 
 export const useWalletConnectWallet = () => {
   const { setAddress } = useAddress();
@@ -11,16 +16,23 @@ export const useWalletConnectWallet = () => {
     id: 1,
     name: 'WalletConnect',
     connect: async () => {
-      const projectId = '5a13f1a5297da2cd768519079890e4fe';
-
       const provider = await EthereumProvider.init({
         chains: [1],
         projectId,
+        events: ['chainChanged', 'accountsChanged'],
+        methods: ['eth_requestAccounts'],
         rpcMap: {
           1: 'https://cloudflare-eth.com',
         },
         showQrModal: true,
       });
+
+      provider.on('display_uri', (uri: string) => {
+        console.log('URI: ', uri);
+      });
+
+      await provider.connect();
+
       const walletClient = createWalletClient({
         chain: mainnet,
         transport: custom(provider),
