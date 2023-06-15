945fc2289c6177c77df149531fa1d50dbce264dc
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-06-16 03:37:38 +0530

    Add error handling for `getChain`

--- package/src/utils/utils.tsx
+++ package/src/utils/utils.tsx
@@ -6,14 +6,19 @@ import * as chains from 'viem/chains';
  * @param chainId - Chain id of the target EVM chain.
  * @returns Viem's chain object.
  */
-export function getChain(chainId: number): chains.Chain {
-  for (const chain of Object.values(chains)) {
-    if (chain.id === chainId) {
-      return chain;
+export function getChain(chainId: number): chains.Chain | undefined {
+  try {
+    for (const chain of Object.values(chains)) {
+      if (chain.id === chainId) {
+        return chain;
+      }
+      if (chainId === 0) return chains.mainnet;
     }
-    if (chainId === 0) return chains.mainnet;
+  } catch (error) {
+    // this error indicates that the chain user has switched to is not supported by viem
+    // TODO: handle this error using native error component
+    throw new Error(`Chain with id ${chainId} not found in viem`);
   }
-  throw new Error(`Chain with id ${chainId} not found`);
 }
 
 export function storePrevWallet(wallet: 'MetaMask' | 'WalletConnect') {
