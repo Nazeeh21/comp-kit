b9f6a3294af6beb1225d567cdcb6b712e2fd93ae
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-07-06 22:00:46 +0530

    Listen to more events from WalletConnect `provider`

--- package/src/components/Wallets/WalletConnect/walletConnectWallet.tsx
+++ package/src/components/Wallets/WalletConnect/walletConnectWallet.tsx
@@ -1,6 +1,7 @@
 import { EthereumProvider } from '@walletconnect/ethereum-provider';
-import { createWalletClient, custom } from 'viem';
+import { Address, createWalletClient, custom } from 'viem';
 import { mainnet } from 'viem/chains';
+import { useEffect } from 'react';
 import {
   useDisconnect,
   useSetAddress,
@@ -24,7 +25,6 @@ import {
   storePrevAccount,
   storePrevWallet,
 } from '../../../utils/utils';
-import { useEffect } from 'react';
 
 interface useWalletConnectWalletProps {
   onClose?: () => void;
@@ -33,128 +33,145 @@ interface useWalletConnectWalletProps {
 export const useWalletConnectWallet = ({
   onClose,
 }: useWalletConnectWalletProps) => {
-  const setWalletClient = useSetWalletClient();
   const projectId = useProjectId();
-
-  const setAddress = useSetAddress(),
-    setConnecting = useSetWalletConnecting(),
-    setError = useSetConnectWalletError(),
-    setWalletProvider = useSetWalletProvider();
-
   const supportedChains = useSupportedChains();
-  const setCurrentChain = useSetCurrentChain();
-
   const currentChain = useCurrentChain();
+  const setWalletClient = useSetWalletClient();
+  const setAddress = useSetAddress();
+  const setConnecting = useSetWalletConnecting();
+  const setError = useSetConnectWalletError();
+  const setWalletProvider = useSetWalletProvider();
+  const setCurrentChain = useSetCurrentChain();
   const disconnect = useDisconnect();
 
   useEffect(() => {
-    // this reconnects the wallet on reloads if the user has connected using WalletConnect previously
-    const prevAccountData = getPrevAccount();
-    const accounts = prevAccountData?.data.accounts;
-    const storedChainData = prevAccountData?.data.chain;
+    const reconnectWalletConnect = async () => {
+      const prevAccountData = getPrevAccount();
+      const prevWallet = getPrevWallet();
+      const accounts = prevAccountData?.data.accounts;
+      const storedChainData = prevAccountData?.data.chain;
 
-    const reconnect = async () => {
-      try {
-        if (
-          getPrevWallet() === 'WalletConnect' &&
-          accounts &&
-          storedChainData
-        ) {
-          const provider = await EthereumProvider.init({
-            chains: [...supportedChains.map(chain => chain.id)],
-            projectId,
-            showQrModal: false,
-            optionalChains: [
-              supportedChains[0].id,
-              ...supportedChains.slice(1).map(chain => chain.id),
-            ],
-          });
-
-          setCurrentChain?.(getChain(storedChainData.id));
-          await provider.connect();
-          provider.on('chainChanged', (chainId: string) => {
-            console.log('chainChanged after reconnect', chainId);
-            setCurrentChain?.(getChain(+chainId));
-          });
-
-          const walletClient = createWalletClient({
-            chain: currentChain ?? mainnet,
-            transport: custom(provider),
-          });
-
-          setWalletProvider('WalletConnect');
-          setWalletClient(walletClient);
-        }
-      } catch (error) {
-        console.log(
-          'Error while automatically re-connecting WalletConnect',
-          error
-        );
-        disconnect();
-        if (error instanceof Error) {
-          throw new Error(error.message);
-        } else {
-          throw new Error(
-            'Error while automatically re-connecting WalletConnect'
-          );
-        }
+      if (prevWallet !== 'WalletConnect' || !accounts || !storedChainData) {
+        return;
       }
-    };
 
-    getPrevWallet() === 'WalletConnect' && void reconnect();
-  }, []);
-
-  return {
-    id: 1,
-    name: 'WalletConnect',
-    connect: async () => {
-      setConnecting(true);
       try {
         const provider = await EthereumProvider.init({
-          chains: [...supportedChains.map(chain => chain.id)],
+          chains: supportedChains.map(chain => chain.id),
           projectId,
-          showQrModal: true,
+          showQrModal: false,
           optionalChains: [
             supportedChains[0].id,
             ...supportedChains.slice(1).map(chain => chain.id),
           ],
         });
 
-        provider.on('display_uri', (uri: string) => {
-          console.log('URI: ', uri);
-        });
+        setCurrentChain?.(getChain(storedChainData.id));
+        await provider.connect();
 
         provider.on('chainChanged', (chainId: string) => {
           setCurrentChain?.(getChain(+chainId));
         });
 
-        await provider.connect();
+        provider.on('accountsChanged', accounts => {
+          setAddress(accounts as Address[]);
+        });
+
+        provider.on('disconnect', () => {
+          disconnect();
+        });
 
         const walletClient = createWalletClient({
           chain: currentChain ?? mainnet,
           transport: custom(provider),
         });
 
-        if (!walletClient) {
-          return;
-        }
         setWalletProvider('WalletConnect');
         setWalletClient(walletClient);
-
-        storePrevWallet('WalletConnect');
-        const accounts = await walletClient.requestAddresses();
-        storePrevAccount({
-          accounts,
-          chain: currentChain ?? supportedChains[0],
-        });
-        setAddress(accounts);
-        onClose?.();
-      } catch (error: unknown) {
-        console.log('Error while connecting WalletConnect', error);
-        setError(error as Error);
-      } finally {
-        setConnecting(false);
+      } catch (error) {
+        console.log(
+          'Error while automatically re-connecting WalletConnect',
+          error
+        );
+        disconnect();
+        throw new Error(
+          error instanceof Error
+            ? error.message
+            : 'Error while automatically re-connecting WalletConnect'
+        );
       }
-    },
+    };
+
+    void reconnectWalletConnect();
+  }, []);
+
+  const connectWalletConnect = async () => {
+    setConnecting(true);
+
+    try {
+      const provider = await EthereumProvider.init({
+        chains: supportedChains.map(chain => chain.id),
+        projectId,
+        showQrModal: true,
+        optionalChains: [
+          supportedChains[0].id,
+          ...supportedChains.slice(1).map(chain => chain.id),
+        ],
+      });
+
+      provider.on('display_uri', (uri: string) => {
+        console.log('URI: ', uri);
+      });
+
+      await provider.connect();
+
+      provider.on('chainChanged', (chainId: string) => {
+        setCurrentChain?.(getChain(+chainId));
+      });
+
+      provider.on('accountsChanged', accounts => {
+        setAddress(accounts as Address[]);
+      });
+
+      provider.on('disconnect', () => {
+        disconnect();
+      });
+
+      const walletClient = createWalletClient({
+        chain: currentChain ?? mainnet,
+        transport: custom(provider),
+      });
+
+      if (!walletClient) {
+        return;
+      }
+
+      setWalletProvider('WalletConnect');
+      setWalletClient(walletClient);
+
+      storePrevWallet('WalletConnect');
+      const accounts = await walletClient.requestAddresses();
+      storePrevAccount({
+        accounts,
+        chain: currentChain ?? supportedChains[0],
+      });
+      setAddress(accounts);
+      onClose?.();
+    } catch (error) {
+      console.log('Error while connecting WalletConnect', error);
+      setError(
+        error instanceof Error
+          ? error
+          : new Error('Error while connecting WalletConnect')
+      );
+    } finally {
+      setConnecting(false);
+    }
+  };
+
+  return {
+    id: 1,
+    name: 'WalletConnect',
+    connect: connectWalletConnect,
   };
 };
